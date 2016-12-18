308ef5766679974792ff1b4c7be8492a7595648e
GanapathyRajalingam <urgans82>
2016-12-18 17:20:00 +0530

    Updated JSON Source
    Updated JSON Source

--- dndTree.js
+++ dndTree.js
@@ -27,7 +27,7 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 
 // Get JSON data
-treeJSON = d3.json("http://localhost:8888/team.json", function(error, treeData) {
+treeJSON = d3.json("team.json", function(error, treeData) {
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
